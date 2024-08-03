0ff32bcdc94f1faa53d0f74e5f3b8b404e88ec27
jinxin.li@hotmail.com <jinxin.li@hotmail.com>
2024-08-02 23:07:38 -0400

    ooini

---
+++ demo0.py
@@ -0,0 +1,14 @@
+
+#_*_coding:utf-8_*_
+# This is a sample Python script.
+
+
+def print_demo(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_demo('Let‘s get something from the website "https://de.products.erstegroup.com/Retail/en/MarketsAndTrends/Overview/index.phtml"\n')
+
